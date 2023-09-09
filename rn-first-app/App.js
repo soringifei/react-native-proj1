ec3059b9cc4e96a255c7aef266937864aaba5283
GBerGG <sgifei@yahoo.com>
2023-09-09 21:27:50 +0300

    23 Course Goal

--- rn-first-app/App.js
+++ rn-first-app/App.js
@@ -1,20 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={{ padding: 30 }}>
+			<View>
+				<TextInput
+					placeholder="Course Goal"
+					style={{ borderColor: 'black', borderWidth: 1 }}
+				/>
+				<Button title="ADD" />
+			</View>
+			<View></View>
+		</View>
+	);
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
