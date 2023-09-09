cd204ba3d524cde249526e2c23f4d90d1cf884e9
GBerGG <sgifei@yahoo.com>
2023-09-09 22:21:14 +0300

    26-Inline-Styles-and-StyleSheets

--- rn-first-app/App.js
+++ rn-first-app/App.js
@@ -3,23 +3,31 @@ import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
 	return (
-		<View style={{ padding: 50 }}>
-			<View
-				style={{
-					flexDirection: 'row',
-					justifyContent: 'space-between',
-					alignItems: 'center',
-				}}
-			>
-				<TextInput
-					placeholder="Course Goal"
-					style={{ width: '80%', borderColor: 'black', borderWidth: 1 }}
+		<View style={styles.screen}>
+			<View style={styles.inputContainer}>
+				<TextInput placeholder="Course Goal" />
+				<Button
+					style={styles.input}
+					title="ADD"
 				/>
-				<Button title="ADD" />
 			</View>
-			<View></View>
 		</View>
 	);
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	screen: { padding: 50 },
+	inputContainer: {
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+		borderColor: 'black',
+		borderWidth: 1,
+	},
+	input: {
+		width: '80%',
+		borderColor: 'black',
+		borderWidth: 1,
+		padding: 10,
+	},
+});
